8dbe14441cde994dd6fd324d03867112b76d063c
aditya27 <120127383+Edgeshot27@users.noreply.github.com>
2025-01-11 23:32:12 +0530

    Delete supplier-compliance-dashboard directory

--- supplier-compliance-dashboard/src/api/compliance.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const BASE_URL = 'https://api.example.com';
-
-export const uploadComplianceData = async (data) => {
-  const response = await axios.post(`${BASE_URL}/compliance`, data);
-  return response.data;
-};
--- supplier-compliance-dashboard/src/utils/formatters.js
+++
@@ -1,5 +0,0 @@
-export const formatCurrency = (amount) =>
-  new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-  }).format(amount);
